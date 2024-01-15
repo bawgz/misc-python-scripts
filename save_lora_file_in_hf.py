76b3879cf65bc4bc34c55456b6cf3f7472188302
Luke Boggs <lukeboggs7@gmail.com>
2024-01-15 12:46:04 -0600

    enable passing args to save_lora_file_in_hf

--- save_lora_file_in_hf.py
+++ save_lora_file_in_hf.py
@@ -1,9 +1,19 @@
 from huggingface_hub import HfApi
+import sys
 
-api = HfApi()
-api.upload_file(
-    path_or_fileobj="dripglasses-pit_viper_sunglasses/model/pit_viper_sunglasses-000003.safetensors",
-    path_in_repo="pit_viper_sunglasses-000003.safetensors",
-    repo_id="bawgz/dripglasses_lora",
-    repo_type="model",
-)
+
+def main(path_to_file, path_in_repo, repo_id, repo_type):
+    api = HfApi()
+    api.upload_file(
+        path_or_fileobj=path_to_file,
+        path_in_repo=path_in_repo,
+        repo_id=repo_id,
+        repo_type=repo_type,
+    )
+
+if __name__ == "__main__":
+    path_to_file = sys.argv[1]
+    path_in_repo = sys.argv[2]
+    repo_id = sys.argv[3]
+    repo_type = sys.argv[4]
+    main(path_to_file, path_in_repo, repo_id, repo_type)
