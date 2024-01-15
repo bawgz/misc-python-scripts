cb19f304de68c12968e303f11b16d69f6f4ef9a6
Luke Boggs <lukeboggs7@gmail.com>
2024-01-15 13:11:07 -0600

    Add file to load model and predict

---
+++ load_model_and_predict.py
@@ -0,0 +1,19 @@
+import torch
+from diffusers import DiffusionPipeline
+
+def main():
+    pipe = DiffusionPipeline.from_pretrained(
+        "bawgz/dripfusion-base",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    ).to("cuda")
+
+    images = pipe("A photo of a man wearing pit viper sunglasses").images
+    # your output image
+    images[0]
+
+
+
+if __name__ == "__main__":
+    main()
