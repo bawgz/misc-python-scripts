d8698271b6730032aee88f7afdf880bfd768b854
Luke Boggs <lukeboggs7@gmail.com>
2024-01-15 16:04:51 -0600

    wtf is happening in this world????????????????

--- load_model_and_predict.py
+++ load_model_and_predict.py
@@ -3,7 +3,7 @@ from diffusers import DiffusionPipeline
 
 def main():
     pipe = DiffusionPipeline.from_pretrained(
-        pretrained_model_name_or_path="bawgz/dripfusion-base",
+        "bawgz/dripfusion-base",
         torch_dtype=torch.float16,
         use_safetensors=True,
         variant="fp16",
