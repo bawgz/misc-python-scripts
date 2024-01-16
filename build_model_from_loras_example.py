aa6581e3576999d735ddcb89e1e5444c844353c1
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 10:30:17 -0600

    low_cpu_mem_usage hell NAH!!!!

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -2,11 +2,11 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
-pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
+pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
 
-# pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy")
+pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-# pipe.fuse_lora(lora_scale=1.0)
+pipe.fuse_lora()
 
 prompt = "toy_face of a hacker with a hoodie"
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
@@ -15,7 +15,7 @@ image.save("output.png")
 
 pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
 
-pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16").to("cuda")
+pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output2.png")
