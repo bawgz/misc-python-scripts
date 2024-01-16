bcd28cd97ad9e25d482ecfcbb89097835da354c3
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 11:18:54 -0600

    wtf!!!!!!!!!!!

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -1,25 +1,21 @@
 from diffusers import DiffusionPipeline
 import torch
 
-from contextlib import redirect_stdout
+pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
+pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
 
-with open('out.txt', 'w') as f:
-    with redirect_stdout(f):
-        pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
-        pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-        pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
+pipe.fuse_lora()
 
-        pipe.fuse_lora()
+prompt = "toy_face of a hacker with a hoodie"
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-        prompt = "toy_face of a hacker with a hoodie"
-        image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+image.save("output.png")
 
-        image.save("output.png")
+pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
 
-        pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
+pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-        pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
-        image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
-
-        image.save("output2.png")
+image.save("output2.png")
