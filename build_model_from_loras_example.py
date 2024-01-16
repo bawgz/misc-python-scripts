c52fb1971b1b375908da7afabb85d20e217fa5eb
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 10:14:26 -0600

    try with just base model to ensure issue is with fusing lora to model

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -4,9 +4,9 @@ import torch
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
 pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
 
-pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy")
+# pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy")
 
-pipe.fuse_lora(lora_scale=1.0)
+# pipe.fuse_lora(lora_scale=1.0)
 
 prompt = "toy_face of a hacker with a hoodie"
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
