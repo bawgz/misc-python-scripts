00fa0217a4b91ca287936767e035c4141f6ffad5
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 11:48:16 -0600

    simplify the loading and saving

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -2,7 +2,7 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
-pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
 
 pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
@@ -13,11 +13,9 @@ image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).ima
 
 image.save("output.png")
 
-pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
+pipe.save_pretrained("../pretrained")
 
-pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
-
-print("?????????????????")
 
 image.save("output2.png")
