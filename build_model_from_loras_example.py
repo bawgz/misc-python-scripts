43ece1fc909791a18f07643e3bc5b6797a46b37b
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 10:55:22 -0600

    gimme the logs

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -1,21 +1,25 @@
 from diffusers import DiffusionPipeline
 import torch
 
-pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
-pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+from contextlib import redirect_stdout
 
-pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
+with open('out.txt', 'w') as f:
+    with redirect_stdout(f):
+        pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
+        pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
 
-pipe.fuse_lora()
+        pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-prompt = "toy_face of a hacker with a hoodie"
-image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+        pipe.fuse_lora()
 
-image.save("output.png")
+        prompt = "toy_face of a hacker with a hoodie"
+        image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
+        image.save("output.png")
 
-pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
-image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+        pipe.save_pretrained("../pretrained", variant="fp16", safe_serialization=True)
 
-image.save("output2.png")
+        pipe = DiffusionPipeline.from_pretrained("../pretrained", torch_dtype=torch.float16, variant="fp16", low_cpu_mem_usage=False).to("cuda")
+        image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+
+        image.save("output2.png")
