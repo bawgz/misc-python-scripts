ae2c12ba4634563aea778d5f0e9e933387d6e4e7
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 13:05:30 -0600

    can I cache realvisxl

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -2,11 +2,15 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
-pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
+pipe = DiffusionPipeline.from_pretrained(
+    pretrained_model_name_or_path="SG161222/RealVisXL_V3.0",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+).to("cuda")
+# pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
-
-pipe.fuse_lora()
+# pipe.fuse_lora()
 
 prompt = "toy_face of a hacker with a hoodie"
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
@@ -15,7 +19,13 @@ image.save("output.png")
 
 pipe.save_pretrained("../pretrained")
 
-pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
+pipe = DiffusionPipeline.from_pretrained(
+    "../pretrained",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+).to("cuda")
+
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output2.png")
