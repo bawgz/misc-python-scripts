88b3ffe256e668c1d8d0b2d65b9fe6301f276837
Luke Boggs <lukeboggs7@gmail.com>
2024-01-17 11:39:55 -0600

    create scripts for testing with and without peft quickly

---
+++ load_cached_model.py
@@ -0,0 +1,9 @@
+from diffusers import DiffusionPipeline
+import torch
+
+prompt = "toy_face of a hacker with a hoodie"
+
+pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+
+image.save("output2.png")
