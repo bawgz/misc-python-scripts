c40a31702644f8358282c7c9b06a012e4e9b689a
Luke Boggs <lukeboggs7@gmail.com>
2024-01-17 14:55:20 -0600

    simplify logs to see summary

--- cache_model.py
+++ cache_model.py
@@ -4,15 +4,15 @@ import torch
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
 pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
 
-print("pipe created", pipe)
+print("pipe created")
 
 pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy")
 
-print("pipe LoRA loaded", pipe)
+print("pipe LoRA loaded")
 
 pipe.fuse_lora()
 
-print("pipe fused", pipe)
+print("pipe fused")
 
 prompt = "toy_face of a hacker with a hoodie"
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
@@ -21,4 +21,4 @@ image.save("output.png")
 
 pipe.save_pretrained("../pretrained")
 
-print("pipe saved", pipe)
+print("pipe saved")
