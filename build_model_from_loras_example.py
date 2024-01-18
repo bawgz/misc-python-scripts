1758bd25b0a3a1f425140fdc9ec78cf00505e1e6
Luke Boggs <lukeboggs7@gmail.com>
2024-01-18 15:11:24 -0600

    add more logs

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -6,7 +6,16 @@ prompt = "toy_face of a hacker with a hoodie"
 
 pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
 
+unet_keys_at_create = pipe.unet.state_dict().keys()
+
+print("unet keys at create", unet_keys_at_create)
+
+
 pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
+
+unet_keys_at_load_lora = pipe.unet.state_dict().keys()
+
+print("unet keys at load lora", unet_keys_at_load_lora)
 
 pipe.fuse_lora()
 
