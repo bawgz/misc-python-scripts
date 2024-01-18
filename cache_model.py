51bffdec1c29b5953a8f7e70d35bc7862ca75a4d
Luke Boggs <lukeboggs7@gmail.com>
2024-01-18 10:25:13 -0600

    back to fusing

--- cache_model.py
+++ cache_model.py
@@ -12,15 +12,15 @@ pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetens
 print("pipe LoRA loaded")
 # print(pipe.unet)
 
-# pipe.fuse_lora()
+pipe.fuse_lora()
 
-# print("pipe fused")
+print("pipe fused")
 # print(pipe.unet)
 
-# prompt = "toy_face of a hacker with a hoodie"
-# image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+prompt = "toy_face of a hacker with a hoodie"
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-# image.save("output.png")
+image.save("output.png")
 
 pipe.save_pretrained("../pretrained")
 
