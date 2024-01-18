5c12311af3ac84f289babc8cb416d5f3c8fa6b95
Luke Boggs <lukeboggs7@gmail.com>
2024-01-17 18:25:05 -0600

    much noise

--- load_cached_model.py
+++ load_cached_model.py
@@ -3,7 +3,7 @@ import torch
 
 prompt = "toy_face of a hacker with a hoodie"
 
-pipe = DiffusionPipeline.from_pretrained("../pretrained", low_cpu_mem_usage=False).to("cuda")
+pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output2.png")
