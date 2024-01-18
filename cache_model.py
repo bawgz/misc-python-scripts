de2250f60d214eee8e3bb70d5da8bb19fd413783
Luke Boggs <lukeboggs7@gmail.com>
2024-01-18 11:22:38 -0600

    getting in the weeds

--- cache_model.py
+++ cache_model.py
@@ -12,10 +12,20 @@ pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetens
 print("pipe LoRA loaded")
 print(pipe.unet)
 
+state_dict_before_fuse = pipe.unet.state_dict().copy()
+
+print("state dict before fuse", state_dict_before_fuse)
+
 pipe.fuse_lora()
 
 print("pipe fused")
 print(pipe.unet)
+
+state_dict_after_fuse = pipe.unet.state_dict().copy()
+
+print("state dict after fuse", state_dict_before_fuse)
+
+print("state dict before fuse == state dict after fuse", state_dict_before_fuse == state_dict_after_fuse)
 
 prompt = "toy_face of a hacker with a hoodie"
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
