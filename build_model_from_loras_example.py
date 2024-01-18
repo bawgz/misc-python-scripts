09347668b23d8596f676f2aa3d171ab6badf647d
Luke Boggs <lukeboggs7@gmail.com>
2024-01-18 09:50:55 -0600

    can you save and load after loading lora

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -2,16 +2,17 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe_id = "stabilityai/stable-diffusion-xl-base-1.0"
+prompt = "toy_face of a hacker with a hoodie"
+
 pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
 
 pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-pipe.fuse_lora()
+# pipe.fuse_lora()
 
-prompt = "toy_face of a hacker with a hoodie"
-image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+# image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-image.save("output.png")
+# image.save("output.png")
 
 pipe.save_pretrained("../pretrained")
 
