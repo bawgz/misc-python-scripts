94d61e915d39c62474595bbfd6d0c11eaf4c4413
Luke Boggs <lukeboggs7@gmail.com>
2024-01-19 10:12:42 -0600

    try unloading lora weights as suggested

--- build_model_from_loras_example.py
+++ build_model_from_loras_example.py
@@ -6,26 +6,14 @@ prompt = "toy_face of a hacker with a hoodie"
 
 pipe = DiffusionPipeline.from_pretrained(pipe_id).to("cuda")
 
-unet_keys_at_create = pipe.unet.state_dict().keys()
-
-print("unet keys at create", unet_keys_at_create)
-
-
 pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetensors", adapter_name="toy", low_cpu_mem_usage=False)
 
-unet_keys_at_load_lora = pipe.unet.state_dict().keys()
-
-print("unet keys at load lora", unet_keys_at_load_lora)
-
 pipe.fuse_lora()
+pipe.unload_lora_weights()
 
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output.png")
-
-unet_keys_at_save = pipe.unet.state_dict().keys()
-
-print("unet keys at save", unet_keys_at_save)
 
 pipe.save_pretrained("../pretrained")
 
@@ -33,3 +21,8 @@ pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output2.png")
+
+pipe.unfuse_lora()
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
+
+image.save("output3.png")
