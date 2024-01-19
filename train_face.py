519f32d7768c22b5e892f7d7f67cde4b03a9fa24
Luke Boggs <lukeboggs7@gmail.com>
2024-01-19 16:39:32 -0600

    update some scripts

--- train_face.py
+++ train_face.py
@@ -1,11 +1,11 @@
 import replicate
 
 training = replicate.trainings.create(
-    version="bawgz/dripfusion-base:30f6d904d186c357901faf3429fecac93af142c449fbafe8bc5c2249ced608c0",
+    version="bawgz/dripfusion-base:614e1f80f6119ad02a601a9f5f8b1bffddff2f66fc42ad54f84158a6b53d7fdd",
     input={
         "input_images": "https://replicate.delivery/pbxt/KAFzTB7svN2l7vJaiwbfL7rVzjSiijDGpCHDKhp8qBKSQjVR/me2.zip",
         "caption_prefix": 'A photo of TOK man, ',
-        "use_face_detection_instead": True,
+        # "use_face_detection_instead": True,
         "train_batch_size": 1,
         "max_train_steps": 4000,
         "lora_lr": 1e-4,
