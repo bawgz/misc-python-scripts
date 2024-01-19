497cf162c56856378c2acae32c2684cfaf9823ba
Luke Boggs <lukeboggs7@gmail.com>
2024-01-19 11:11:59 -0600

    let's see that mf unet

--- load_cached_model.py
+++ load_cached_model.py
@@ -4,6 +4,10 @@ import torch
 prompt = "toy_face of a hacker with a hoodie"
 
 pipe = DiffusionPipeline.from_pretrained("../pretrained").to("cuda")
+
+print("pipe created")
+print(pipe.unet)
+
 image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
 image.save("output2.png")
