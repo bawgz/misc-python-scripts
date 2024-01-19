b1d00ace7ec00d1470292d0ae8ae8ea1cb0f4638
Luke Boggs <lukeboggs7@gmail.com>
2024-01-19 10:42:26 -0600

    restore cache model to its former simplicity

--- cache_model.py
+++ cache_model.py
@@ -12,34 +12,16 @@ pipe.load_lora_weights("CiroN2022/toy-face", weight_name="toy_face_sdxl.safetens
 print("pipe LoRA loaded")
 # print(pipe.unet)
 
-state_dict_before_fuse = pipe.unet.state_dict().copy()
-
 pipe.fuse_lora()
 
 print("pipe fused")
 # print(pipe.unet)
 
-state_dict_after_fuse = pipe.unet.state_dict().copy()
 
-def dict_compare(d1, d2):
-    d1_keys = set(d1.keys())
-    d2_keys = set(d2.keys())
-    shared_keys = d1_keys.intersection(d2_keys)
-    added = d1_keys - d2_keys
-    removed = d2_keys - d1_keys
-    modified = {o : (d1[o], d2[o]) for o in shared_keys if not torch.all(torch.eq(d1[o], d2[o]))}
-    return added, removed, modified
+prompt = "toy_face of a hacker with a hoodie"
+image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
 
-added, removed, modified = dict_compare(state_dict_before_fuse, state_dict_after_fuse)
-
-print("added:", added)
-print("removed:", removed)
-print("modified:", modified)
-
-# prompt = "toy_face of a hacker with a hoodie"
-# image = pipe(prompt, num_inference_steps=30, generator=torch.manual_seed(0)).images[0]
-
-# image.save("output.png")
+image.save("output.png")
 
 pipe.save_pretrained("../pretrained")
 
